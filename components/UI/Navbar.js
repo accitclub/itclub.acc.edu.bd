794adc349f9c4810e044032a0b55bdad728febb3
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-02-20 00:16:48 +0600

    Transition Group added to navigation

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -1,6 +1,6 @@
-import {Disclosure} from '@headlessui/react'
+import {Disclosure, Transition} from '@headlessui/react'
 import {MenuIcon, XIcon} from '@heroicons/react/outline'
-import {useEffect, useState} from "react";
+import {useEffect, useState, Fragment} from "react";
 
 import {HERO_ID, NAVIGATION as navigation} from "../../assets/data";
 import Logo from "./Images";
@@ -37,9 +37,9 @@ export default function Navbar() {
             <Disclosure as="nav" className={classNames("transition-all bg-bgDark fixed w-screen z-50", navBg)}>
                 {({open}) => (
                     <>
-                        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                        <div className="max-w-7xl mx-auto px-2 md:px-6 lg:px-8">
                             <div className="relative flex items-center justify-between h-16">
-                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                                     {/* Mobile menu button*/}
                                     <Disclosure.Button
                                         className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -51,11 +51,11 @@ export default function Navbar() {
                                         )}
                                     </Disclosure.Button>
                                 </div>
-                                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
                                     <div className="flex-shrink-0 flex items-center">
                                         <Logo/>
                                     </div>
-                                    <div className="hidden sm:block sm:ml-6">
+                                    <div className="hidden md:block sm:ml-6">
                                         <div className="flex space-x-4">
                                             {navigation.map((item) => (
                                                 <a
@@ -76,24 +76,27 @@ export default function Navbar() {
                             </div>
                         </div>
 
-                        <Disclosure.Panel className="sm:hidden transition-all">
-                            <div className="px-2 pt-2 pb-3 space-y-1">
-                                {navigation.map((item) => (
-                                    <Disclosure.Button
-                                        key={item.name}
-                                        as="a"
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white',
-                                            'block px-3 py-2 rounded-md text-md font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </Disclosure.Button>
-                                ))}
-                            </div>
-                        </Disclosure.Panel>
+                        <Transition enterFrom={"transform -translate-y-20 duration-500"} leaveFrom={"opacity-1"} leaveTo={"opacity-0"} show={open} as={Fragment}>
+                            <Disclosure.Panel className="md:hidden transition-all">
+                                    <div className="px-2 pt-2 pb-3 space-y-1">
+                                        {navigation.map((item) => (
+                                            <Disclosure.Button
+                                                key={item.name}
+                                                as="a"
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white',
+                                                    'block px-3 py-2 rounded-md text-md font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </Disclosure.Button>
+                                        ))}
+                                    </div>
+
+                            </Disclosure.Panel>
+                        </Transition>
                     </>
                 )}
             </Disclosure>
