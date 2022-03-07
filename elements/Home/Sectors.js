8f6dddc5c1fd795c31cc2b69801eebbe21acc423
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-07 10:29:58 +0600

    Sector redesign

--- elements/Home/Sectors.js
+++ elements/Home/Sectors.js
@@ -1,4 +1,4 @@
-import {Center, Container, Content, FlexRow, Section} from "../../components/UI/Containers";
+import {Center, Container, Content, Section} from "../../components/UI/Containers";
 import {Wave} from "../../components/UI/Images";
 import {CenterText, Heading, HorizontalWatermark, P, ResponsiveTextLeft, Text} from "../../components/UI/Typography";
 import {
@@ -18,7 +18,7 @@ export default function Sectors() {
         <Section id={SECTOR_ID}>
             <Content className={"overflow-hidden bg-white"}>
                 <Container>
-                    <FlexRow className={"justify-between md:flex-row flex-col"}>
+                    <Content className={"grid lg:grid-cols-2"}>
                         <Center className={"flex-[4]"}>
                             <Content>
                                 <motion.div className={"relative"}
@@ -38,36 +38,51 @@ export default function Sectors() {
                                             {SECTOR_CONTENT_TEXT}
                                         </Text>
                                         <AnchorButton href={"#" + TESTIMONIAL_ID}
-                                                      className={"w-56 xl:mt-20 bg-green-600 lg:mt-20 md:mt-20 text-white mt-10 md:mr-auto md:mx-0 mx-auto"}>
+                                                      className={"lg:flex hidden w-56 xl:mt-20 bg-emerald-500 lg:mt-20 md:mt-20 text-white mt-10 md:mr-auto md:mx-0 mx-auto"}>
                                             Learn More
                                         </AnchorButton>
                                     </ResponsiveTextLeft>
                                 </motion.div>
                             </Content>
                         </Center>
-                        <Content className={"h-full flex-[3] md:ml-auto md:mt-0 mt-16 "}>
+                        <Content className={"h-full lg:ml-auto lg:mt-0 mt-16 "}>
                             <motion.div
-                                className={"md:flex md:flex-rowh -full grid grid-cols-3 sm:gap-4 gap-1 md:flex-grow md:flex-wrap md:justify-end  md:transform md:rotate-12"}>
+                                className={"grid lg:grid-cols-2 sm:grid-cols-3 grid-cols-2 sm:gap-x-8 gap-x-4 gap-y-8 md:justify-items-end justify-items-center"}>
                                 {SECTOR_LIST.map((each, key) => (
-                                    <motion.div initial={{opacity: 0, y: 10}}
-                                                whileInView={{opacity: 1, y: 0}}
+                                    <motion.div initial={{opacity: 0, y: 10, borderColor: "#fff"}}
+                                                whileInView={{opacity: 1, y: 0, borderColor: ""}}
                                                 key={key}
                                                 transition={{delay: key * 0.2}}
                                                 viewport={{once: true}}
-                                                className={"grid place-items-center bg-slate-900  xl:h-56 xl:w-56 lg:h-44 lg:w-44 md:h-32 md:w-32 sm:h-44 sm:w-full xs:h-32 xs:w-full sxs:h-28 sxs:w-full h-24 w-full  md:m-3 mx-auto"}>
-                                        <CenterText className={"text-xl"}>
-                                            <Center
-                                                className={classNames("xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl", key % 3 === 0 ? "text-emerald-400" : key % 3 === 1 ? "text-blue-400" : "text-yellow-300")}>
-                                                {each.icon}
-                                            </Center>
-                                            <P className={"xl:text-xl text-white font-semibold text-white lg:text-lg md:text-base sm:text-xl text-sm"}>{each.name}</P>
-
-                                        </CenterText>
+                                                className={classNames("border-8 rounded-full",
+                                                    key % 3 === 0 ? "border-emerald-100" : key % 3 === 1 ? "border-indigo-100" : "border-orange-100",
+                                                    "grid place-items-center xl:h-60 xl:w-60 lg:h-56 lg:w-56 md:h-52 ",
+                                                    "md:w-52 sm:h-48 sm:w-48 xs:h-40 xs:w-40 h-40 w-40 md:m-3 mx-auto")}>
+                                        <Content className={classNames("w-full h-full grid place-items-center",
+                                            "border-4 rounded-full border-indigo-500",
+                                            key % 3 === 0 ? "border-emerald-400" : key % 3 === 1 ? "border-indigo-400" : "border-orange-400"
+                                        )}>
+                                            <Content
+                                                className={classNames("w-full h-full grid place-items-center relative",
+                                                    "border-8 rounded-full",
+                                                    key % 3 === 0 ? "border-emerald-100" : key % 3 === 1 ? "border-indigo-100" : "border-orange-100",
+                                                )}>
+                                                <CenterText className={"text-xl"}>
+                                                    <Center
+                                                        className={classNames("xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl", key % 3 === 0 ? "text-emerald-400" : key % 3 === 1 ? "text-blue-400" : "text-yellow-300")}>
+                                                        {each.icon}
+                                                    </Center>
+                                                    <P className={"xl:text-xl text-gray-600 font-semibold text-white lg:text-lg md:text-base sm:text-xl text-sm"}>{each.name}</P>
+                                                </CenterText>
+                                                <span
+                                                    className={"absolute px-4 w-20 py-4 rounded-full bg-white -bottom-6"}/>
+                                            </Content>
+                                        </Content>
                                     </motion.div>
                                 ))}
                             </motion.div>
                         </Content>
-                    </FlexRow>
+                    </Content>
                 </Container>
             </Content>
             <Wave className={"text-white bg-bgDark"}/>
