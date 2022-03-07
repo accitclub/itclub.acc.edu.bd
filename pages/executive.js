e4f3127b43b7780a6781945471505f97f45d2117
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-07 11:01:40 +0600

    Executive page added

--- pages/executive.js
+++ pages/executive.js
@@ -1,16 +1,23 @@
-import {Main, Section} from "../components/UI/Containers";
 import Navbar from "../components/UI/Navbar";
 import Footer from "../elements/Home/Footer";
 import HTMLHead from "../components/HTMLHead";
-import Hero from "../elements/Executive/ExecutiveHero";
+import HeroComponent from "../elements/HeroComponent";
+import HeroImage from "../assets/images/hero.png";
+import HeroCenterImage from "../assets/images/herocenter.png";
+import {ABOUT_ID, HERO_TEXT, HERO_TITLE_1, HERO_TITLE_2} from "../assets/data";
 
-export default function Executive(){
-    return(
+export default function Executive() {
+    return (
         <>
             <HTMLHead/>
             <Navbar/>
             <main className={"lg:pt-0 pt-10"}>
-               <Hero/>
+                <HeroComponent HeroImage={HeroImage} HeroCenterImage={HeroCenterImage}
+                               HeroText={"ACC IT Club has a talented and very special panel. Meet our panel members"}
+                               HeroTitle={HERO_TITLE_1 + " IT Club"}
+                               HeroSecondaryTitle={"Panel Members"}
+                               Href={""}
+                               CTA={"Learn more"}/>
             </main>
             <Footer/>
         </>
