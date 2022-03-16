9f1b50a52cc45059ce9e18c38b753842fdf0c162
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-16 23:46:37 +0600

    Panel Data added

---
+++ assets/data/panel.js
@@ -0,0 +1,80 @@
+import Pushpita from "../images/executives/21/21_pushpita.jpeg";
+import Abrar from "../images/executives/21/21_abrar.jpeg";
+import Sifar from "../images/executives/21/21_sifar.jpeg";
+import Sakin from "../images/executives/21/21_sakin.jpeg";
+import Nahian from "../images/executives/21/21_nahian.jpeg";
+import Mujahid from "../images/executives/21/21_mujahid.jpeg";
+import Ratul from "../images/executives/21/21_ratul.jpeg";
+import Omar from "../images/executives/21/21_omar.jpeg";
+import AFM from "../images/executives/21/21_afm.jpeg";
+import Afina from "../images/executives/21/21_afina.jpeg";
+import Rageeb from "../images/executives/21/21_ragib.jpg";
+import Anas from "../images/executives/21/21_anas.png";
+
+export const PanelMembersList = [
+    {
+        title: "21-22",
+        members: [
+            {
+                name: "Pushpita Saha",
+                image: Pushpita,
+                text: "Administrator",
+            },
+            {
+                name: "Abrar Amir Hossein",
+                image: Abrar,
+                text: "Co-Administrator",
+            },
+            {
+                name: "Sifar Mahfuz",
+                image: Sifar,
+                text: "Management Lead",
+            },
+            {
+                name: "Kazi Mostafa Sakin",
+                image: Sakin,
+                text: "Organizing Lead",
+            },
+            {
+                name: "Nahian Nasir",
+                image: Nahian,
+                text: "Marketing Lead",
+            },
+            {
+                name: "Mujahid Mahi",
+                image: Mujahid,
+                text: "Public Relations Officer",
+            },
+            {
+                name: "Raisul Islam Ratul",
+                image: Ratul,
+                text: "Graphics Design Lead",
+            },
+            {
+                name: "Anas Uddin",
+                image: Anas,
+                text: "Gaming Lead",
+            },
+            {
+                name: "Omar Azraf",
+                image: Omar,
+                text: "Gaming Co-Lead",
+            },
+            {
+                name: "AFM Zareef Ahnaf",
+                image: AFM,
+                text: "App Development Lead",
+            },
+            {
+                name: "Ragib Shahriar",
+                image: Rageeb,
+                text: "Programming Lead",
+            },
+            {
+                name: "Afina Tahia",
+                image: Afina,
+                text: "Programming Co-Lead",
+            },
+        ]
+    }
+]
